12b616ef09ab1501eb26b7b06cba3fa3f8eca4ae
kturchik <kturchik@sbcglobal.net>
2018-04-03 00:52:28 -0700

    Added shrinking navbar

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 0)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+    
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(function() {
+    if ($("#mainNav").offset().top > 26) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  });
+    
+
+  //Carousel
   /*Pauses the Carousel whenever hover over with pointer in order to read the Speaker Bio otherwise it will move to the next item due to the time interval*/
   $('.carousel').carousel(function () {
     this.pause(true);
   });
-
+    
   //Filter data
   //https://bootsnipp.com/snippets/r1Z7d
   $(document).ready(function(){
