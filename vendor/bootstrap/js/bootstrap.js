3c33fb6f5d2a024bfc4044354ba4f9b26a3e4beb
kturchik <kturchik@sbcglobal.net>
2018-04-15 01:20:42 -0700

    Slowed down speaker cards a little more

--- vendor/bootstrap/js/bootstrap.js
+++ vendor/bootstrap/js/bootstrap.js
@@ -554,7 +554,7 @@ var Carousel = function ($) {
   var TOUCHEVENT_COMPAT_WAIT = 500; // Time for mouse compat events to fire after touch
 
   var Default = {
-    interval: 15000,
+    interval: 20000,
     keyboard: true,
     slide: false,
     pause: 'hover',
